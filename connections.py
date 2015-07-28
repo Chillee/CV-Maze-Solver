be60a8512b692e57af5c9ba6f6e7f29626f21de1
leeb3 <leeb3@cmc306-12.mathcs.carleton.edu>
2015-07-28 16:35:58 -0500

    Refactored to conform with pep8

--- connections.py
+++ connections.py
@@ -1,6 +1,6 @@
 import numpy as np
-import sys
 import cv2
+
 
 def bresenham_line(a, b):
     x0, y0 = a
@@ -13,25 +13,26 @@ def bresenham_line(a, b):
     if dx > dy:
         err = dx / 2.0
         while x != x1:
-            yield (x,y)
+            yield (x, y)
             err -= dy
             if err < 0:
-                y += sy
+                y += y
                 err += dx
             x += sx
     else:
         err = dy / 2.0
         while y != y1:
-            yield (x,y)
+            yield (x, y)
             err -= dx
             if err < 0:
                 x += sx
                 err += dy
             y += sy
 
-def check_LOS(img, a, b):
-    for x, y, in bresenham_line(a,b):
-        if img[y,x] == 0:
+
+def check_los(img, a, b):
+    for x, y, in bresenham_line(a, b):
+        if img[y, x] == 0:
             return False
     return True
 
@@ -39,33 +40,40 @@ num_clicks = 0
 start_ = None
 end_ = None
 
-def get_connections(g, skeleton, eroded, img):
-    hier, contours, hierarchy = cv2.findContours(skeleton.copy(),cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE )
 
-    blank_image = np.zeros((img.shape[0], img.shape[1],3), np.uint8)
-    areas = [(cv2.contourArea(c), index) for index,c in enumerate(contours)]
+def get_connections(g, skeleton, eroded, img):
+    hier, contours, hierarchy = cv2.findContours(skeleton.copy(),
+                                                 cv2.RETR_CCOMP,
+                                                 cv2.CHAIN_APPROX_SIMPLE)
+
+    blank_image = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
+    areas = [(cv2.contourArea(c), index) for index, c in enumerate(contours)]
     areas = sorted(areas, reverse=True)
 
     tree = g.build_kd_tree()
 
     for i in areas:
         contour = contours[i[1]]
-        blank_image = cv2.drawContours(blank_image, [contour], 0, (255, 255, 255), 1)
+        blank_image = cv2.drawContours(blank_image, [contour], 0,
+                                       (255, 255, 255), 1)
 
-        [vx,vy,x,y] = cv2.fitLine(contour, cv2.DIST_L2,0,0.01,0.01)
+        [vx, vy, x, y] = cv2.fitLine(contour, cv2.DIST_L2, 0, 0.01, 0.01)
 
         cols, row, depth = img.shape
 
-        arcLength = cv2.arcLength(contour, True)/2
-        x0, y0 = float(x+arcLength*vx*.5), float(y+arcLength*vy*.5)
-        x1, y1 = float(x-arcLength*vx*.5), float(y-arcLength*vy*.5)
+        arc_length = cv2.arcLength(contour, True) / 2
+        x0, y0 = float(x + arc_length * vx * .5), \
+            float(y + arc_length * vy * .5)
+        x1, y1 = float(x - arc_length * vx * .5), \
+            float(y - arc_length * vy * .5)
 
         nodes = []
-        node_pos, nodes = tree.query(np.array([[x0,y0], [x1,y1]]))
+        node_pos, nodes = tree.query(np.array([[x0, y0], [x1, y1]]))
         node_a = nodes[0]
         node_b = nodes[1]
-        if check_LOS(eroded, g.nodes[node_a].pos, g.nodes[node_b].pos):
+        if check_los(eroded, g.nodes[node_a].pos, g.nodes[node_b].pos):
             dx = g.nodes[node_a].pos[0] - g.nodes[node_b].pos[0]
             dy = g.nodes[node_a].pos[1] - g.nodes[node_b].pos[1]
             g.link_nodes(node_a, node_b, np.sqrt(dx * dx + dy * dy))
-            cv2.line(img, g.nodes[node_a].pos, g.nodes[node_b].pos, (0,255,0), 1, cv2.LINE_AA)
+            cv2.line(img, g.nodes[node_a].pos, g.nodes[node_b].pos,
+                     (0, 255, 0), 1, cv2.LINE_AA)
--- graph.py
+++ graph.py
@@ -2,10 +2,12 @@ import numpy as np
 import scipy.spatial
 from Queue import PriorityQueue
 
+
 class Node(object):
     def __init__(self, pos):
         self.pos = pos
         self.connections = []
+
 
 class Graph(object):
     def __init__(self):
@@ -22,12 +24,13 @@ class Graph(object):
     def link_nodes(self, a, b, d=1):
         self.nodes[a].connections.append((b, d))
         self.nodes[b].connections.append((a, d))
-        self.connections.append((a,b, d))
+        self.connections.append((a, b, d))
 
     def build_kd_tree(self):
         if self.tree_changed or self.tree is None:
             self.tree_chnaged = False
-            self.tree = scipy.spatial.KDTree(np.array([list(node.pos) for node in self.nodes]))
+            self.tree = scipy.spatial.KDTree(
+                np.array([list(node.pos) for node in self.nodes]))
         return self.tree
 
     def dijkstra(self, start, end):
@@ -35,23 +38,22 @@ class Graph(object):
         dists[start] = 0
         prev = [-1 for i in self.nodes]
 
-        curNodes = PriorityQueue()
-        curNodes.put((0, start))
+        cur_nodes = PriorityQueue()
+        cur_nodes.put((0, start))
 
-        while not curNodes.empty():
-            curDist, index = curNodes.get()
+        while not cur_nodes.empty():
+            curDist, index = cur_nodes.get()
 
             for connection in self.nodes[index].connections:
-                newDist = dists[index] + connection[1]
-                if newDist < dists[connection[0]]:
-                    dists[connection[0]] = newDist
+                new_dist = dists[index] + connection[1]
+                if new_dist < dists[connection[0]]:
+                    dists[connection[0]] = new_dist
                     prev[connection[0]] = index
-                    curNodes.put((newDist, connection[0]))
+                    cur_nodes.put((new_dist, connection[0]))
 
-        curNode = end
-        path =[]
-        while curNode != -1:
-            path.append(curNode)
-            curNode = prev[curNode]
+        cur_node = end
+        path = []
+        while cur_node != -1:
+            path.append(cur_node)
+            cur_node = prev[cur_node]
         return path[::-1]
-
