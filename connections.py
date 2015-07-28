917b3acfcafabe0a4f93ef0b3ddc98b7eed67067
Benjamin Lee <benjamin.fik.lee@gmail.com>
2015-07-28 17:50:03 -0500

    Better connections algorithm

--- connections.py
+++ connections.py
@@ -16,7 +16,7 @@ def bresenham_line(a, b):
             yield (x, y)
             err -= dy
             if err < 0:
-                y += sy
+                y += y
                 err += dx
             x += sx
     else:
@@ -31,45 +31,57 @@ def bresenham_line(a, b):
 
 
 def check_los(img, a, b):
+    print(a, b)
     for x, y, in bresenham_line(a, b):
+        print(x, y)
         if img[y, x] == 0:
             return False
     return True
 
+num_clicks = 0
+start_ = None
+end_ = None
+
 
 def get_connections(g, skeleton, eroded, img):
-    hier, contours, hierarchy = cv2.findContours(skeleton.copy(),
-                                                 cv2.RETR_CCOMP,
-                                                 cv2.CHAIN_APPROX_SIMPLE)
-
-    blank_image = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
-    areas = [(cv2.contourArea(c), index) for index, c in enumerate(contours)]
-    areas = sorted(areas, reverse=True)
-
+    skeleton = cv2.resize(skeleton, (0, 0), fx=0.5, fy=0.5)
+    height, width = skeleton.shape
     tree = g.build_kd_tree()
 
-    for i in areas:
-        contour = contours[i[1]]
-        blank_image = cv2.drawContours(blank_image, [contour], 0,
-                                       (255, 255, 255), 1)
+    visited = cv2.merge((skeleton, skeleton * 0, skeleton * 0))
+    for node in g.nodes:
+        cv2.rectangle(visited,
+                      (int(node.pos[0] / 2) - 1, int(node.pos[1] / 2) - 1),
+                      (int(node.pos[0] / 2) + 1, int(node.pos[1] / 2) + 1),
+                      (255, 255, 0), -1)
+        #visited[node.pos[1] / 4, node.pos[0] / 4][1] = 255
 
-        [vx, vy, x, y] = cv2.fitLine(contour, cv2.DIST_L2, 0, 0.01, 0.01)
+    pixel_stack = [((g.nodes[0].pos[0] / 2, g.nodes[0].pos[1] / 2), 0)]
+    i = 0
+    while len(pixel_stack) > 0:
+        i += 1
+        pos, cur_node = pixel_stack.pop()
+        visited[pos[1], pos[0]][0] = 0
+        if i % 1000 == 0:
+            cv2.imshow("visited", visited)
+            cv2.waitKey(1)
 
-        cols, row, depth = img.shape
+        if visited[pos[1], pos[0]][1] != 0:
+            node_dists, nodes = tree.query(np.array([[pos[0] * 2, pos[1] * 2]]))
+            node = nodes[0]
+            if node != cur_node:
+                if cur_node not in [e[0] for e in g.nodes[node].connections]:
+                    dx = g.nodes[node].pos[0] - g.nodes[cur_node].pos[0]
+                    dy = g.nodes[node].pos[1] - g.nodes[cur_node].pos[1]
+                    cv2.line(img, g.nodes[node].pos, g.nodes[cur_node].pos,
+                             (0, 255, 0), 1, cv2.LINE_AA)
+                    g.link_nodes(node, cur_node, np.sqrt(dx * dx + dy * dy))
+                cur_node = node
 
-        arc_length = cv2.arcLength(contour, True) / 2
-        x0, y0 = float(x + arc_length * vx * .5), \
-            float(y + arc_length * vy * .5)
-        x1, y1 = float(x - arc_length * vx * .5), \
-            float(y - arc_length * vy * .5)
-
-        nodes = []
-        node_pos, nodes = tree.query(np.array([[x0, y0], [x1, y1]]))
-        node_a = nodes[0]
-        node_b = nodes[1]
-        if check_los(eroded, g.nodes[node_a].pos, g.nodes[node_b].pos):
-            dx = g.nodes[node_a].pos[0] - g.nodes[node_b].pos[0]
-            dy = g.nodes[node_a].pos[1] - g.nodes[node_b].pos[1]
-            g.link_nodes(node_a, node_b, np.sqrt(dx * dx + dy * dy))
-            cv2.line(img, g.nodes[node_a].pos, g.nodes[node_b].pos,
-                     (0, 255, 0), 1, cv2.LINE_AA)
+        for dx in range(-1, 2):
+            for dy in range(-1, 2):
+                x = pos[0] + dx
+                y = pos[1] + dy
+                if x >= 0 and x < width and y >= 0 and y < height:
+                    if visited[y, x][0] != 0:
+                        pixel_stack.append(((x, y), cur_node))
