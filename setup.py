91ba35fced2e87bddd401265c4f2a66580538cf6
Benjamin Lee <benjamin.fik.lee@gmail.com>
2015-07-30 16:29:22 -0500

    Added tile-based skeletonization which is much faster on some images

--- setup.py
+++ setup.py
@@ -7,9 +7,13 @@ extensions = [
     Extension("connections", ["connections.pyx"],
               include_dirs=[numpy.get_include()],
               extra_compile_args=["-O3"],
+              extra_link_args=["-O3"]),
+    Extension("skeletonize", ["skeletonize.pyx"],
+              include_dirs=[numpy.get_include()],
+              extra_compile_args=["-O3"],
               extra_link_args=["-O3"])
 ]
 
 setup(
-    ext_modules=cythonize(extensions)
+    ext_modules=cythonize(extensions, annotate=True)
 )
